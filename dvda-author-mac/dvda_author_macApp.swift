1b3752af5ae6dd4e5635d8d2f47f1064c0f4bd5b
Yusuke Ito <novi.mad@gmail.com>
2024-07-22 19:36:39 +0900

    prepare UI app

---
+++ dvda-author-mac/dvda_author_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  dvda_author_macApp.swift
+//  dvda-author-mac
+//
+//  Created by Yusuke Ito on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct dvda_author_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
